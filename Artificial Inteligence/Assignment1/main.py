68b1832af1a98466fd462be1c3959ea9f721b45a
Raducu Vlad-Rares <vlad101vlad@gmail.com>
2021-02-24 21:59:29 +0200

    Added the necessary resources

--- Artificial Inteligence/Assignment1/main.py
+++ Artificial Inteligence/Assignment1/main.py
@@ -1,4 +1,11 @@
 from Domain.Environment import Environment
+from Domain.Map import DMap
+
+import pygame
 
 if __name__ == '__main__':
     enviroment = Environment()
+    map = DMap()
+
+    pygame.init()
+
